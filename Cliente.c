393aecdefb471cc2ed7fec6001c3bccffc262380
dot-neko <lx.neko@gmail.com>
2019-06-22 18:50:26 -0300

    0.18a Faltaban include para close call

--- Cliente.c
+++ Cliente.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <Socket_Cliente.h>
 #include <Socket.h>
 
--- Servidor.c
+++ Servidor.c
@@ -9,6 +9,8 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
 
 main ()
 {
